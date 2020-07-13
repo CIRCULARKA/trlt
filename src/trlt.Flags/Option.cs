68ac3b08fc3a642603f3e9629fc149e8c4513942
CIRCULARKA <donkardon@list.ru>
2020-07-13 19:27:51 +0500

    Constructor added to Option class

--- src/trlt.Flags/Option.cs
+++ src/trlt.Flags/Option.cs
@@ -2,6 +2,13 @@ namespace trlt.Options
 {
 	class Option
 	{
+		public Option(string _short, string full, string description)
+		{
+			ShortName = _short;
+			FullName = full;
+			Description = description;
+		}
+
 		public string ShortName { get; private set; }
 		public string FullName { get; private set; }
 		public string Description { get; private set; }
