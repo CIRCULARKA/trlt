2445b1cc9944ce3033a5a5d29c0a64230029593a
CIRCULARKA <donkardon@list.ru>
2020-07-13 19:39:33 +0500

    Valid options added

--- src/trlt.Flags/ValidOptions.cs
+++ src/trlt.Flags/ValidOptions.cs
@@ -2,6 +2,15 @@ namespace trlt.Options
 {
 	class ValidOptions
 	{
-
+		public Option[] GetValidOptions =>
+			new Option[]
+			{
+				new Option("-h", "--help", "Show this help."),
+				new Option("-V", "--Version", "Show application version."),
+				new Option("-s", "--repl-spaces", "Replace spaces with underscores."),
+				new Option("-r", "--recursive", "Transliterate all files in directory recursively."),
+				new Option(".", "", "Transliterate all files in current directory."),
+				new Option("..", "", "Transliterate all files in upper directory.")
+			};
 	}
 }
