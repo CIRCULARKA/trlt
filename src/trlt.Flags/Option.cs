f020b2e23153eda5791cdb30af3c242f0f9c0a89
CIRCULARKA <donkardon@list.ru>
2020-07-13 19:26:34 +0500

    Option class filled with option options

--- src/trlt.Flags/Option.cs
+++ src/trlt.Flags/Option.cs
@@ -2,6 +2,9 @@ namespace trlt.Options
 {
 	class Option
 	{
+		public string ShortName { get; private set; }
+		public string FullName { get; private set; }
+		public string Description { get; private set; }
 
 	}
 }
