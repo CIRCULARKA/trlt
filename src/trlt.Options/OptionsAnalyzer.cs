63cbb862e5d8ca09668062de5bbfb3aab0a15ca8
CIRCULARKA <donkardon@list.ru>
2020-07-14 14:58:16 +0500

    Added method to check options

--- src/trlt.Options/OptionsAnalyzer.cs
+++ src/trlt.Options/OptionsAnalyzer.cs
@@ -2,11 +2,32 @@ namespace trlt.Options
 {
 	class OptionsAnalyzer
 	{
-		string[] _args;
+		string[] args;
+		ValidOptions valid_options;
+		OptionsActivator activate;
 
-		public OptionsAnalyzer(string[] args)
+		public OptionsAnalyzer(string[] _args)
 		{
-			_args = args;
+			args = _args;
+			valid_options = new ValidOptions();
+			activate = new OptionsActivator();
+
+		}
+
+		bool IsValidOption(string _option)
+		{
+			foreach (Option option in valid_options.GetValidOptions)
+				if (_option == option.ShortName || _option == option.FullName)
+					return true;
+			return false;
+		}
+
+		public void CheckForOptions()
+		{
+			foreach (string arg in args)
+			{
+
+			}
 		}
 	}
 }
