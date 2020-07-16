2c1418a9d8313297ceeab7a7f0e84d14e4c2b83c
CIRCULARKA <donkardon@list.ru>
2020-07-17 01:32:16 +0500

    Class renamed

---
+++ src/trlt.Containers/UserMessagesSender.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace trlt.Containers
+{
+	class UserMessagesSender
+	{
+		public void ShowSuccessMsg(string value)
+        {
+            ConsoleColor native_color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(value);
+            Console.ForegroundColor = native_color;
+        }
+
+		public void ShowErrMsg(string value)
+        {
+            ConsoleColor native_color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+            Console.ForegroundColor = native_color;
+        }
+
+		 public void ShowLogMsg(string value)
+        {
+            Console.WriteLine(value);
+        }
+	}
+}
