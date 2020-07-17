c9ce44d577ea83a3307dfdc2d841e63ff53e5a91
CIRCULARKA <donkardon@list.ru>
2020-07-17 14:03:44 +0500

    ActivateOption method update

--- src/trlt.Options/OptionsActivator.cs
+++ src/trlt.Options/OptionsActivator.cs
@@ -1,3 +1,4 @@
+using System;
 using trlt.Containers;
 
 namespace trlt.Options
@@ -7,19 +8,32 @@ namespace trlt.Options
 		ValidOptions valid_option;
 		UserMessagesSender msg;
 
+		string ver, utility_name, author, github;
+
 		public OptionsActivator()
 		{
 			valid_option = new ValidOptions();
 			msg = new UserMessagesSender();
+
+			ver = "v1.0";
+			utility_name = "trlt";
+			author = "CIRCULARKA";
+			github = "https://github.com/CIRCULARKA/trlt";
 		}
 
 		public void ActivateOption(string name)
 		{
-			if (name == valid_option.GetOption(OptionName.HelpOption).FullName |
-				name == valid_option.GetOption(OptionName.HelpOption).FullName)
+			if (name == valid_option.GetOption(OptionName.HelpOption))
 			{
 				msg.ShowLogMsg("This program allows you to transliterate filenames or strings from cyrillic to english."
-					+ "\n\nUse:\ntrlt [option] [value]");
+					+ "\n\nUse:\ntrlt [option] [value].\nAvailable options:\n");
+				foreach (Option op in valid_option.GetValidOptions)
+					Console.WriteLine($"\t{op.ShortName}\t{op.FullName}\t{op.Description}");
+			}
+			else if (name == valid_option.GetOption(OptionName.VerOption))
+			{
+				msg.ShowLogMsg($"{utility_name} {ver}. Author: CIRCULARKA.\n" +
+					$"Github: {github}.");
 			}
 		}
 	}
